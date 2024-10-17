0a8fb96f581092a47dfb30caf4b639cebc58d958
LucassMau <lucas13matu@gmail.com>
2024-10-17 10:21:07 -0300

    Add files via upload

---
+++ TPO_Grupo13_Baranczuk_Maturano_Otta_PerezLingua NUEVA VERSION CON NOTACION DE CAMBIOS POR HACER.py
@@ -0,0 +1,5 @@
+# FUNCION generarId(usuarios)
+# FUNCION añadir(elemento, lista, datos, generos=None):#usamos la misma funcion para usuario, genero y libro
+#FUNCION eliminar(elemento, lista, id):#usamos la misma funcion para usuario, genero y libro
+#FUNCION ver(elemento, lista):#usamos la misma funcion para usuario, genero y libro
+#FUNCION prestarLibro(usuarios, libros, prestamos):
